feec941a94a27dfa69117023fdef31eb40f80c67
aaanX11 <aaa1n1istp7r89w@gmail.com>
2017-11-07 03:44:04 +0300

    auth

--- foodsharing/urls.py
+++ foodsharing/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import url, include
 from . import views
 from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^$', views.list_supplies, name='list_supplies'),
@@ -9,6 +11,8 @@ urlpatterns = [
     url(r'^/list_suggestions/$', views.list_suggestions, name='list_suggestions'),
     url(r'^supply/(?P<pk>\d+)/$', views.supply, name='supply'),
     url(r'^suggestion/(?P<pk>\d+)/$', views.suggestion, name='suggestion'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
 
 if settings.DEBUG:
